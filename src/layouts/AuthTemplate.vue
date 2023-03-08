c4803f6ae24d0c8cf5f885e3cf0d744ef8a81fe4
Izaias Neto <izaiasmneto@gmail.com>
2023-03-08 11:58:20 -0300

    Criar página de autenticação

---
+++ src/layouts/AuthTemplate.vue
@@ -0,0 +1,71 @@
+<template>
+  <section
+    id="loginPage"
+    :style="{
+      backgroundImage: 'url(' + require('@/assets/images/bgLogin.jpg') + ')',
+    }"
+  >
+    <div class="loginContent">
+      <div class="loginCard">
+        <div
+          class="decor"
+          style="background-image: url('./assets/images/building.jpg')"
+        >
+          <div class="content">
+            <span class="logo">
+              <img
+                :src="require('@/assets/images/logo.svg')"
+                alt="EspecializaTi"
+              />
+            </span>
+            <span class="dots">
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
+            <span class="description">
+              <p>
+                Os melhores e mais completos cursos de Laravel do Brasil, cursos
+                com projetos reais. Do zero ao profissional.
+              </p>
+            </span>
+            <span class="copyright fontSmall">
+              Todos os Direitos reservados - <b>Especializati</b>
+            </span>
+          </div>
+        </div>
+        <div class="login">
+          <div class="content">
+            <span class="logo"
+              ><img :src="require('@/assets/images/logoDark.svg')" alt="" />
+            </span>
+            <span>
+              <p>Seja muito bem vindo(a)!</p>
+            </span>
+            <span class="dots">
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
+            <span class="description">
+              Acesse nossa plataforma e desfrute de cursos completos para sua
+              especialização.
+            </span>
+
+            <router-view></router-view>
+            
+          </div>
+          <span class="copyright fontSmall">
+            Todos os Direitos reservados - <b>Especializati</b>
+          </span>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "AuthTemplate",
+};
+</script>
---
+++ src/views/auth/ForgetPassword.vue
@@ -0,0 +1,36 @@
+<template>
+  <form action="/dist/index.html" method="">
+    <div class="groupForm">
+      <i class="far fa-envelope"></i>
+      <input
+        type="email"
+        name="email"
+        placeholder="E-mail"
+        v-model="email"
+        required
+      />
+    </div>
+    <button
+      :class="['btn', 'primary', loading ? 'loading' : '']"
+      type="submit"
+      @click.prevent="forgetPassword"
+    >
+      <span v-if="loading">Recuperando...</span>
+      <span v-else>Recuperar Senha</span>
+    </button>
+  </form>
+  <span>
+    <p class="fontSmall">
+      Acessar?
+      <router-link :to="{ name: 'auth' }" class="link primary"
+        >Clique aqui</router-link
+      >
+    </p>
+  </span>
+</template>
+
+<script>
+export default {
+  name: "ForgetPassword",
+};
+</script>
