9fbd349085f56a0afc02983efdbae4a37978cef8
Izaias Neto <izaiasmneto@gmail.com>
2023-03-08 14:21:07 -0300

    Criar Services

---
+++ src/services/AuthService.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+import { URL_API } from "@/configs"
+
+export default class AuthService {
+
+    static async auth(params) {
+        return axios.post(`${URL_API}/auth`, params)
+    }
+}
--- src/views/auth/Auth.vue
+++ src/views/auth/Auth.vue
@@ -9,7 +9,7 @@
       <input type="password" name="password" placeholder="Senha" required />
       <i class="far fa-eye buttom"></i>
     </div>
-    <button class="btn primary" type="submit" @click.prevent="login">Login</button>
+    <button class="btn primary" type="submit" @click.prevent="auth">Login</button>
   </form>
   <span>
     <p class="fontSmall">
@@ -22,15 +22,24 @@
 </template>
 
 <script>
-import router from '@/router'
+//import router from '@/router'
+import { useStore } from 'vuex'
 
 export default {
   name: "Auth",
   setup() {
-    const login = () => router.push({name: 'campus.home'})
+    const store = useStore()
+
+    const auth = () => {
+      store.dispatch('auth', {
+          email: 'izaiasmneto@gmail.com',
+          password: '123456',
+          device_name: 'authbyvue3'
+      })
+    }
 
     return {
-      login
+      auth
     }
   }
 };
