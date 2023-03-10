c7e058b1d3e6fe5e65c291612e6be809dbc62919
Izaias Neto <izaiasmneto@gmail.com>
2023-03-10 14:09:32 -0300

    Add Menu Component

---
+++ src/layouts/components/MenuComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <transition name="fade">
+            <ul id="mainMenu">
+                <li>
+                   <router-link :to="{name: 'campus.home'}">Início</router-link>
+                </li>
+                <li>
+                   <router-link :to="{name: 'campus.my.supports'}">Minhas Dúvidas</router-link>
+                </li>
+                <li>
+                  <a href="">Sair</a>
+                </li>
+            </ul>
+        </transition>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'MenuComponent'
+}
+</script>
