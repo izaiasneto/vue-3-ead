b0600ccf1d74849d0b8efbcbdf9ef401b6b71a5b
Izaias Neto <izaiasmneto@gmail.com>
2023-03-10 17:13:25 -0300

    animations css

--- src/layouts/components/MenuComponent.vue
+++ src/layouts/components/MenuComponent.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
         <transition name="fade">
-            <ul id="mainMenu">
-                <li>
-                   <router-link :to="{name: 'campus.home'}">Início</router-link>
+            <ul id="mainMenu" >
+                <li class="animate__animated animate__headShake">
+                   <router-link :to="{name: 'campus.home'} ">Início</router-link>
                 </li>
-                <li>
+                <li class="animate__animated animate__headShake">
                    <router-link :to="{name: 'campus.my.supports'}">Minhas Dúvidas</router-link>
                 </li>
-                <li>
+                <li class="animate__animated animate__headShake">
                   <a href="#" @click.prevent="logout">
                         <span v-if="loadingStore">Saindo...</span>
                         <span v-else>Sair</span>
